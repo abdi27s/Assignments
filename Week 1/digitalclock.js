810adc97e650f09f82580cb8687e4557fafcc91e
abdi27s <debitmaharjan12@gmail.com>
2020-12-03 17:06:13 +0545

    all done

--- Week 1/digitalclock.js
+++ Week 1/digitalclock.js
@@ -1,12 +1,12 @@
-setInterval(displayClock, 1000);
+setInterval(displayClock, 1000);  //to recall function after 1000ms
 
 function displayClock() {
   let displayDate = new Date();
-  let hour = displayDate.getHours();
+  let hour = displayDate.getHours();                
   let minute = displayDate.getMinutes();
   let second = displayDate.getSeconds();
 
-  hour = hour < 10 ? "0" + hour : hour;
+  hour = hour < 10 ? "0" + hour : hour;         //to display 1-9 as 01-09 
   minute = minute < 10 ? "0" + minute : minute;
   second = second < 10 ? "0" + second : second;
 
@@ -14,4 +14,3 @@ function displayClock() {
 
   document.getElementById("time").innerHTML = theTime;
 }
-
