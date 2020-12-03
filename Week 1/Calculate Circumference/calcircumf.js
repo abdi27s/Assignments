d4dd124692922c4fa1b24ff4826f6ba234059e22
abdi27s <debitmaharjan12@gmail.com>
2020-12-03 19:40:46 +0545

    updated

--- Week 1/Calculate Circumference/calcircumf.js
+++ Week 1/Calculate Circumference/calcircumf.js
@@ -1,15 +1,17 @@
 function circumference() {
   let r = document.getElementById("radius").value;
-  let pi = Math.PI;
+  let pi = Math.PI; //value of pi.
 
-  let circumf = 2 * pi * r;
-
+  let circumf = 2 * pi * r; //circumference of circle formula.
+  circumf = circumf.toFixed(2); //takes only 2 decimal places.
   circumf =
-    `<p>The Circumference of circle with radius ` +
-    r +
-    `cm is:
-    </p>` +
-    circumf;
+    // `<p>The Circumference of circle with radius ` +
+    // r +
+    // `cm is: ` +
+    // circumf +
+    // ` cm </p>`;
+
+    `<p>The circumference of circle with radius ${r} cm is : ${circumf} cm</p>`
 
   document.getElementById("radius").value = "";
   document.getElementById("circ").innerHTML = circumf;
