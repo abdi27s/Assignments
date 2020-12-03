56b8444722b8bafa1bd99646cd35af4fafaeca37
abdi27s <debitmaharjan12@gmail.com>
2020-12-03 21:46:11 +0545

    updated

--- Week 1/Calculate Circumference/calcircumf.js
+++ Week 1/Calculate Circumference/calcircumf.js
@@ -2,17 +2,21 @@ function circumference() {
   let r = document.getElementById("radius").value;
   let pi = Math.PI; //value of pi.
 
-  let circumf = 2 * pi * r; //circumference of circle formula.
-  circumf = circumf.toFixed(2); //takes only 2 decimal places.
-  circumf =
-    // `<p>The Circumference of circle with radius ` +
-    // r +
-    // `cm is: ` +
-    // circumf +
-    // ` cm </p>`;
+  if (r <= 0) {
+    alert("Please enter the radius.");
+  } else {
+    let circumf = 2 * pi * r; //circumference of circle formula.
+    circumf = circumf.toFixed(2); //takes only 2 decimal places.
+    circumf =
+      // `<p>The Circumference of circle with radius ` +
+      // r +
+      // `cm is: ` +
+      // circumf +
+      // ` cm </p>`;
 
-    `<p>The circumference of circle with radius ${r} cm is : ${circumf} cm</p>`
+      `<p>The circumference of circle with radius ${r} cm is : ${circumf} cm</p>`;
 
-  document.getElementById("radius").value = "";
-  document.getElementById("circ").innerHTML = circumf;
+    document.getElementById("radius").value = "";
+    document.getElementById("circ").innerHTML = circumf;
+  }
 }
--- Week 1/Tip Calculator/tipcalculator.js
+++ Week 1/Tip Calculator/tipcalculator.js
@@ -1,17 +1,22 @@
+function tipCalculation() {
+  let amount = document.getElementById("totalAmount").value;
+  let percent = document.getElementById("percentage").value;
 
+  if (amount <= 0) {
+    alert("Please enter the Bill Amount.");
+    location.reload();
+  } else if (percent <= 0) {
+    alert("Please enter the tip percentage.");
+    location.reload();
+  } else {
+    let tipAmount = (amount * percent) / 100;
 
-function tipCalculation(){
-    let amount=document.getElementById('totalAmount').value;
-    let percent=document.getElementById('percentage').value;
-
-    let tipAmount=amount*percent/100;
-
-
-    tipAmount=`<p>Total Amount: Rs ${amount} <br>
+    tipAmount = `<p>Total Amount: Rs ${amount} <br>
     Tip Percentage: ${percent} %<br>
-    Tip Amount: Rs ${tipAmount}.</p>`
-    
-    document.getElementById('totalAmount').value='';
-    document.getElementById('percentage').value='';
-    document.getElementById('result').innerHTML=tipAmount;
+    Tip Amount: Rs ${tipAmount}.</p>`;
+
+    document.getElementById("totalAmount").value = "";
+    document.getElementById("percentage").value = "";
+    document.getElementById("result").innerHTML = tipAmount;
+  }
 }
