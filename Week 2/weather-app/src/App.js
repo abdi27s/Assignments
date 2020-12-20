d64ea4eb288c73237fd943b2c6f05339bcb20a1b
abdi27s <debitmaharjan12@gmail.com>
2020-12-20 12:48:26 +0545

    init

---
+++ Week 2/weather-app/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const api = {
+  apiKey: "3de9f8d3365267c8c3cf0c932b83e059",
+  apiSite: "api.openweathermap.org/data/2.5/",
+};
+
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        
+      </main>
+    </div>
+  );
+}
+
+export default App;
